d61f3f7a5c469875e036a46a544e5d1ff723c3b4
AkashChandel27 <akashchandel27@gmail.com>
2023-03-11 22:34:51 +0530

    new

--- src/test/java/online/Selenium_Practice/Session_25_Authentication_Popup.java
+++ src/test/java/online/Selenium_Practice/Session_25_Authentication_Popup.java
@@ -8,11 +8,11 @@ public class Session_25_Authentication_Popup {
 	public static void main(String[] args) {
 		
 
-		// Launch chrome browser
+		// Launch chrome browser 
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\akash.chandel.ACS\\Desktop\\Translatation\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
-		// maximise browser
+		// maximise browser's
 		driver.manage().window().maximize();
 
 		// open url:- user id and password in the URL in the application
